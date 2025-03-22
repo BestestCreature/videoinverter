9cc14dce45210cf7058a6e19975bb3382285cd44
BestestCreature <bestcreaturesallowed@hotmail.com>
2025-03-22 22:29:02 +0100

    Update best-creatures-video-inverter.user.js

--- best-creatures-video-inverter.user.js
+++ best-creatures-video-inverter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Best Creatures Video Inverter
+// @name         Best Creatures Video Inverter & Replacer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Inverts video
+// @version      1.1
+// @description  Inverts video or replaces it with an external iframe based on stream title
 // @author       Best Creature
 // @match        https://www.twitch.tv/*
 // @grant        none
@@ -11,7 +11,7 @@
 (function() {
     'use strict';
 
-
+    // Function to toggle video inversion
     function toggleVideoInvert(shouldInvert) {
         const video = document.querySelector('video');
         if (video) {
@@ -25,19 +25,33 @@
         }
     }
 
+    // Function to replace video with external iframe
+    function replaceVideoWithIframe() {
+        const videoPlayer = document.querySelector('.video-player');
+        if (videoPlayer) {
+            videoPlayer.outerHTML = '<iframe src="https://vkvideo.ru/video_ext.php?oid=1037355211&id=456239017" width="100%" height="100%" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>';
+        }
+    }
 
+    // Function to check stream title for specific commands
     function checkStreamTitle() {
         const titleElement = document.querySelector('[data-a-target="stream-title"]');
         if (titleElement) {
             const title = titleElement.getAttribute('title');
-            if (title && title.includes('!script')) {
-                toggleVideoInvert(true);
-            } else {
-                toggleVideoInvert(false);
+            if (title) {
+                if (title.includes('!script')) {
+                    toggleVideoInvert(true);  // Invert video if !script is found
+                } else {
+                    toggleVideoInvert(false); // Reset video if !script is not found
+                }
+
+                if (title.includes('!iq')) {
+                    replaceVideoWithIframe();  // Replace video if !iq is found
+                }
             }
         }
     }
 
-
+    // Run the check every 5 seconds
     setInterval(checkStreamTitle, 5000);
 })();
